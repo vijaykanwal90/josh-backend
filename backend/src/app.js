881ce8c77b828702bffddb0d0c09fc5bd632bedb
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-23 22:58:32 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,20 @@ import authrouter from './routes/auth.router.js'
 import userrouter from './routes/user.router.js'
 import courserouter from './routes/course.router.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 const app = express();
+
 app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionsSuccessStatus: 200,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+
+
+}
+app.user(cors(corsOptions))
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
