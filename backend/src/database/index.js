57a2988bb155959315d41f203bf5059db780748a
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-24 16:36:03 +0530

    added logs

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         console.log(process.env.MONGODB_URL)
+        console.log("Connecting port is ",process.env.PORT)
        const connectionIstance =  await mongoose.connect(`${process.env.MONGODB_URL}/josh-backend`);
        console.log(`\n MongoDB connected !!  DB HOST ${connectionIstance.connection.host}`);
     } catch (error) {
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ dotenv.config({
     path: './.env'
 })
 import connectDB from './database/index.js';
+console.log("MONGODB URL is ",process.env.MONGODB_URL)
+console.log("PORT is ",process.env.PORT)
 connectDB() 
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
