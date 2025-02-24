4a180e8f687df4f12aef03bd61344e09276d294d
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-24 16:07:03 +0530

    added console.log

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        // console.log(process.env.MONGODB_URL)
+        console.log(process.env.MONGODB_URL)
        const connectionIstance =  await mongoose.connect(`${process.env.MONGODB_URL}/josh-backend`);
        console.log(`\n MongoDB connected !!  DB HOST ${connectionIstance.connection.host}`);
     } catch (error) {
