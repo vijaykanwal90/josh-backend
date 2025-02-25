1e60a0fc9a7458f38c8eb2a86c5962952fa54fd4
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-25 17:55:07 +0530

    changed orgin on aws deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const URL = process.env.FRONTEND_URL;
 // console.log(URL)
 app.use(cookieParser());
 const corsOptions = {
-    origin: URL,
+    origin: "http://localhost:5173",
     credentials: true,
     optionsSuccessStatus: 200,
     allowedHeaders: ['Content-Type', 'Authorization'],
