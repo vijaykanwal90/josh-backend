f355e423325fbe943a6a14c8455f90cfaa272bed
vivekrawat21 <vivekrwt2111@gmail.com>
2025-03-03 22:11:57 +0530

    user router and controller updated

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -20,15 +20,25 @@ const getUser = asynchHandler(async (req, res) => {
 
 const updateUser = asynchHandler(async (req, res) => {
     try {
-        const { id } = req.params;
-        const user = await User.findByIdAndUpdate(id, req.body, {
+        const  id  = req.user._id;
+        const {name, mobilenumber,email} = req.body;
+        
+        console.log(name, mobilenumber, email)
+        const user = await User.findByIdAndUpdate(id, {
+            name,
+            mobilenumber,
+            email
+        }, {
             new: true,
             runValidators: true
         });
+
+        console.log(user)
         if (!user) {
             console.log(error)
             throw new ApiError(404, "User not found");
         }
+
         res.status(200).json(new ApiResponse(200, { user }, "User updated successfully"));
     } catch (error) {
         console.log(error)
@@ -37,18 +47,5 @@ const updateUser = asynchHandler(async (req, res) => {
 }
 );
 
-const getUserById = asynchHandler(async (req, res) => {
-    const { id } = req.params;
-    try {
-        const user = await User.findById(id);
-        if (!user) {
-            throw new ApiError(404, "User not found");
-        }
-        res.status(200).json(new ApiResponse(200, { user }, "User fetched successfully"));
-    } catch (error) {
-        console.log(error);
-        throw new ApiError(500, "Internal server error");
-    }
-}
-);
-export { getUser, updateUser, getUserById };
+
+export { updateUser, getUser };
--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getUser, updateUser, getUserById } from "../controllers/user.controller.js";
+import { getUser, updateUser } from "../controllers/user.controller.js";
+import { userAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+router.use(userAuth);
 router.route('/').get(getUser)
-router.route('/:id').get(getUserById).patch(updateUser);;
+router.route('/').patch(updateUser);
 
 export default router;
