6dc89074758e3711f542e052bd49537da5a263b1
vivekrawat21 <vivekrwt2111@gmail.com>
2025-03-10 20:54:36 +0530

    email address lowercase

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,6 +87,7 @@ const loginUser = asynchHandler(async (req, res) => {
     let { email, password } = req.body;
     try {
         email = email.toLowerCase();
+       
         console.log("in logged route")
         const user = await User
             .findOne({ email })
