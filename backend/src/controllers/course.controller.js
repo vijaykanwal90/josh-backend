c4af62b8cf4323b2eba774afeef670948f83a64c
vivekrawat21 <vivekrwt2111@gmail.com>
2025-04-11 23:35:22 +0530

    remove category field from course schema

--- backend/src/controllers/course.controller.js
+++ backend/src/controllers/course.controller.js
@@ -62,110 +62,114 @@ import { Bundle } from "../models/bundle.model.js";
 // });
 const createCourse = asynchHandler(async (req, res) => {
     const {
-      title,
-      image,
-      bundleName,
-      category,
-      description,
-      price,
-      duration,
-      courseMentorName,
-      whyCourse,
-      whatYouWillLearn,
-      courseHighlights,
-      whoShouldEnroll,
-      videos = []
-    } = req.body;
-  
-    try {
-      // Find or create bundle
-      let bundle = await Bundle.findOne({ bundleName });
-  
-      if (!bundle) {
-        bundle = new Bundle({
-          bundleName,
-          courses: []
-        });
-        await bundle.save();
-      }
-  
-      // marking first video as preview true
-      const formattedVideos = videos.map((video, index) => ({
-        title: video.title,
-        url: convertToEmbedUrl(video.url),
-        isPreview: index === 0 ? true : (video.isPreview || false)
-      }));
-  
-      // Create course with videos
-      const course = new Course({
         title,
         image,
-        bundle: bundle._id,
+        bundleName,
         category,
         description,
         price,
         duration,
         courseMentorName,
-        videos: formattedVideos,
         whyCourse,
         whatYouWillLearn,
         courseHighlights,
-        whoShouldEnroll
-      });
-  
-      await course.save();
-  
-      // Add course to bundle
-      bundle.courses.push(course._id);
-      await bundle.save();
-  
-      return res.status(201).json(
-        new ApiResponse(201, { course }, "Course created with videos successfully")
-      );
-  
+        whoShouldEnroll,
+        videos = [],
+        isTrending
+    } = req.body;
+
+    try {
+        // Find or create bundle
+        let bundle = await Bundle.findOne({ bundleName });
+
+        //   if (!bundle) {
+        //     bundle = new Bundle({
+        //       bundleName,
+        //       courses: []
+        //     });
+        //     await bundle.save();
+        //   }
+
+        // marking first video as preview true
+        const formattedVideos = videos.map((video, index) => ({
+            title: video.title,
+            url: convertToEmbedUrl(video.url),
+            isPreview: index === 0 ? true : (video.isPreview || false)
+        }));
+
+        // Create course with videos
+        const course = new Course({
+            title,
+            image,
+            bundleName,
+            description,
+            price,
+            duration,
+            courseMentorName,
+            videos: formattedVideos,
+            whyCourse,
+            whatYouWillLearn,
+            courseHighlights,
+            whoShouldEnroll,
+            isTrending: isTrending || false,
+        });
+
+        await course.save();
+        // Add course to bundle
+        if (bundle) {
+            bundle.courses.push(course._id);
+
+            await bundle.save();
+        }
+
+        return res.status(201).json(
+            new ApiResponse(201, { course }, "Course created with videos successfully")
+        );
+
     } catch (error) {
-      console.error(error);
-      throw new ApiError(500, "Internal server error", error);
+        console.error(error);
+        throw new ApiError(500, "Internal server error", error);
     }
-  });
-  
-  const addVideos = asynchHandler(async (req, res) => {
+});
+
+const addVideos = asynchHandler(async (req, res) => {
     const { courseId } = req.params;
     const { videos = [] } = req.body;
-  
+
     try {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        throw new ApiError(404, "Course not found");
-      }
-  
-      const isCourseEmpty = course.videos.length === 0;
-  
-      const newVideos = videos.map((video, index) => ({
-        title: video.title,
-        url: convertToEmbedUrl(video.url),
-        isPreview: isCourseEmpty && index === 0 // Only the first video if course is empty
-      }));
-  
-      course.videos.push(...newVideos);
-      await course.save();
-  
-      return res
-        .status(200)
-        .json(new ApiResponse(200, { course }, "Videos added successfully"));
+        const course = await Course.findById(courseId);
+        if (!course) {
+            throw new ApiError(404, "Course not found");
+        }
+
+        const isCourseEmpty = course.videos.length === 0;
+
+        const newVideos = videos.map((video, index) => ({
+            title: video.title,
+            url: convertToEmbedUrl(video.url),
+            isPreview: isCourseEmpty && index === 0 // Only the first video if course is empty
+        }));
+
+        course.videos.push(...newVideos);
+        await course.save();
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { course }, "Videos added successfully"));
     } catch (error) {
-      console.error(error);
-      throw new ApiError(500, "Internal server error", error);
+        console.error(error);
+        throw new ApiError(500, "Internal server error", error);
     }
-  });
-  
+});
+
 // Create a new bundle
 const createBundle = asynchHandler(async (req, res) => {
     const {
         bundleName,
         description,
-        oldPrice,
-        price
+        price,
+        whyBundle,
+        bundleImage
     } = req.body;
 
     try {
@@ -174,11 +178,15 @@ const createBundle = asynchHandler(async (req, res) => {
             bundle = new Bundle({
                 bundleName,
                 description,
-                oldPrice,
                 price,
+                whyBundle,
+                bundleImage: bundleImage || "pending",
                 courses: []
             });
             await bundle.save();
+        }
+        else {
+            throw new ApiError(400, "Bundle already exists");
         }
 
         return res.status(201).json(new ApiResponse(201, { bundle }, "Bundle created successfully"));
@@ -227,29 +235,29 @@ const getBundles = asynchHandler(async (req, res) => {
         throw new ApiError(500, "Internal server error", error);
     }
 });
-const getCourseByName = asynchHandler(async(req,res)=>{
-    const {name} = req.body;
+const getCourseByName = asynchHandler(async (req, res) => {
+    const { name } = req.body;
     console.log(name)
-    try{
+    try {
         const course = await Course.find({
-            title: { $regex: name, $options: "i" } 
+            title: { $regex: name, $options: "i" }
         });
 
         if (course.length === 0) {
             throw new ApiError(404, "No bundles matching the name found");
         }
 
-        return res.status(200).json(new ApiResponse(200,{course},"Course fetched successfully"));
+        return res.status(200).json(new ApiResponse(200, { course }, "Course fetched successfully"));
     }
-    catch(error){
+    catch (error) {
         console.error(error);
-        throw new ApiError(500,"Internal server error",error);
+        throw new ApiError(500, "Internal server error", error);
     }
 
 })
-const getBundleByName = asynchHandler(async(req,res)=>{
-    const {name} = req.body;
-    try{
+const getBundleByName = asynchHandler(async (req, res) => {
+    const { name } = req.body;
+    try {
         const bundle = await Bundle.find({
             bundleName: { $regex: name, $options: "i" } // "i" for case-insensitive matching
         });
@@ -258,43 +266,43 @@ const getBundleByName = asynchHandler(async(req,res)=>{
             throw new ApiError(404, "No bundles matching the name found");
         }
 
-        return res.status(200).json(new ApiResponse(200,{bundle},"Bundle fetched successfully"));
+        return res.status(200).json(new ApiResponse(200, { bundle }, "Bundle fetched successfully"));
     }
-    catch(error){
+    catch (error) {
         console.error(error);
-        throw new ApiError(500,"Internal server error",error);
+        throw new ApiError(500, "Internal server error", error);
     }
-    
+
 })
 const getBundleById = asynchHandler(async (req, res) => {
-        const { id } = req.params;
-        try {
-            const bundle = await Bundle
-                .findById(id)
-                .populate("courses");
-            if (!bundle) {
-                throw new ApiError(404, "Bundle not found");
-            }
-            return res.status(200).json(new ApiResponse(200, { bundle }, "Bundle fetched successfully"));
-        } catch (error) {
-            console.error(error);
-            throw new ApiError(500, "Internal server error", error);
+    const { id } = req.params;
+    try {
+        const bundle = await Bundle
+            .findById(id)
+            .populate("courses");
+        if (!bundle) {
+            throw new ApiError(404, "Bundle not found");
         }
+        return res.status(200).json(new ApiResponse(200, { bundle }, "Bundle fetched successfully"));
+    } catch (error) {
+        console.error(error);
+        throw new ApiError(500, "Internal server error", error);
     }
+}
 );
 
 
 const getAllBundles = asynchHandler(async (req, res) => {
-        try {
-            console.log("Fetching all bundles");
-            // Fetch all bundles and populate the courses field
-            const bundles = await Bundle.find().populate("courses");
-            return res.status(200).json(new ApiResponse(200, { bundles }, "Bundles fetched successfully"));
-        } catch (error) {
-            console.error(error);
-            throw new ApiError(500, "Internal server error");
-        }
-    })
+    try {
+        console.log("Fetching all bundles");
+        // Fetch all bundles and populate the courses field
+        const bundles = await Bundle.find().populate("courses");
+        return res.status(200).json(new ApiResponse(200, { bundles }, "Bundles fetched successfully"));
+    } catch (error) {
+        console.error(error);
+        throw new ApiError(500, "Internal server error");
+    }
+})
 
 // Fetch all courses
 const getCourses = asynchHandler(async (req, res) => {
@@ -382,93 +390,93 @@ const deleteCourse = asynchHandler(async (req, res) => {
         throw new ApiError(500, "Internal server error");
     }
 });
-const assignCourse = asynchHandler(async (req,res)=>{
-    const {courseId,studentId} = req.body;
-    try{
+const assignCourse = asynchHandler(async (req, res) => {
+    const { courseId, studentId } = req.body;
+    try {
         console.log(courseId);
         console.log(studentId);
 
         const course = await Course.findById(courseId);
-        if(!course){
-            throw new ApiError(404,"Course not found");
+        if (!course) {
+            throw new ApiError(404, "Course not found");
         }
-        if(course.students.includes(studentId)){
-            throw new ApiError(400,"User already assigned to course");
+        if (course.students.includes(studentId)) {
+            throw new ApiError(400, "User already assigned to course");
         }
         course.students.push(studentId);
         const user = await User.findById(studentId);
-        if(!user){
-            throw new ApiError(404,"User not found");
+        if (!user) {
+            throw new ApiError(404, "User not found");
         }
-        if(user.courses.includes(courseId)){
-            throw new ApiError(400,"Course already assigned to user");
+        if (user.courses.includes(courseId)) {
+            throw new ApiError(400, "Course already assigned to user");
         }
         user.courses.push(courseId);
         await course.save();
         await user.save();
-        return res.status(200).json(new ApiResponse(200, { course,user }, "Course deleted successfully"));
+        return res.status(200).json(new ApiResponse(200, { course, user }, "Course deleted successfully"));
     }
-    catch(error){
+    catch (error) {
         console.log(error)
-        throw new ApiError(500,"Internal server error",error);
+        throw new ApiError(500, "Internal server error", error);
     }
 })
-const assignBundle = asynchHandler(async (req,res)=>{
+const assignBundle = asynchHandler(async (req, res) => {
     // const {userId} = req.body;
     const bundleId = req.body.courseId;
     const userId = req.body.studentId;
-    try{
+    try {
         // console.log(bundleId)
         // console.log(userId)
         const bundle = await Bundle.findById(bundleId);
-        if(!bundle){
-            throw new ApiError(404,"Bundle not found");
+        if (!bundle) {
+            throw new ApiError(404, "Bundle not found");
         }
-        if(bundle.students.includes(userId)){
-            throw new ApiError(400,"User already assigned to course");
+        if (bundle.students.includes(userId)) {
+            throw new ApiError(400, "User already assigned to course");
         }
         bundle.students.push(userId);
         const user = await User.findById(userId);
-        if(!user){
-            throw new ApiError(404,"User not found");
+        if (!user) {
+            throw new ApiError(404, "User not found");
         }
-        if(user.bundles.includes(bundleId)){
-            throw new ApiError(400,"Bundle already assigned to user");
+        if (user.bundles.includes(bundleId)) {
+            throw new ApiError(400, "Bundle already assigned to user");
         }
         user.bundles.push(bundleId);
         const oneLevelUser = await User.findOne({ sharableReferralCode: user.referredByCode });
-        if(!oneLevelUser){
-            throw new ApiError(404,"User not found");
+        if (!oneLevelUser) {
+            throw new ApiError(404, "User not found");
         }
         // one level up user
-        if(oneLevelUser){
+        if (oneLevelUser) {
             oneLevelUser.total_income += bundle.price * 0.25;
-            oneLevelUser.today_income +=  bundle.price * 0.25;
-            oneLevelUser.last_7_days_income +=  bundle.price * 0.25;
-            oneLevelUser.last_30_days_income +=  bundle.price * 0.25;
+            oneLevelUser.today_income += bundle.price * 0.25;
+            oneLevelUser.last_7_days_income += bundle.price * 0.25;
+            oneLevelUser.last_30_days_income += bundle.price * 0.25;
             oneLevelUser.myTeam.push(user._id);
             oneLevelUser.totalTeam += 1;
             await oneLevelUser.save();
         }
         // second level user;
-        const secondLevelUser = await User.findOne({sharableReferralCode: oneLevelUser.referredByCode});
-        if(secondLevelUser){
-            secondLevelUser.today_income +=  bundle.price * 0.10;
+        const secondLevelUser = await User.findOne({ sharableReferralCode: oneLevelUser.referredByCode });
+        if (secondLevelUser) {
+            secondLevelUser.today_income += bundle.price * 0.10;
             secondLevelUser.total_income += secondLevelUser.today_income;
 
-            secondLevelUser.last_7_days_income +=  bundle.price * 0.10;
-            secondLevelUser.last_30_days_income +=  bundle.price * 0.10;
+            secondLevelUser.last_7_days_income += bundle.price * 0.10;
+            secondLevelUser.last_30_days_income += bundle.price * 0.10;
             secondLevelUser.myTeam.push(user._id);
             secondLevelUser.totalTeam += 1;
             await secondLevelUser.save();
         }
         await bundle.save();
         await user.save();
-        return res.status(200).json(new ApiResponse(200, { bundle,user }, "Bundle assigned successfully"));
+        return res.status(200).json(new ApiResponse(200, { bundle, user }, "Bundle assigned successfully"));
     }
-    catch(error){
+    catch (error) {
         console.log(error)
-        throw new ApiError(500,"Error while assigning bundle",error);
+        throw new ApiError(500, "Error while assigning bundle", error);
     }
 })
 
@@ -488,18 +496,17 @@ export {
     assignCourse,
     assignBundle,
     getCourseByName,
-    getBundleByName, 
+    getBundleByName,
     addVideos
 };
 function convertToEmbedUrl(url) {
     try {
-      const parsedUrl = new URL(url);
-      const videoId = parsedUrl.pathname.split('/').pop();
-      const query = parsedUrl.search; // includes ?si=...
-      return `https://www.youtube.com/embed/${videoId}${query}`;
+        const parsedUrl = new URL(url);
+        const videoId = parsedUrl.pathname.split('/').pop();
+        const query = parsedUrl.search; // includes ?si=...
+        return `https://www.youtube.com/embed/${videoId}${query}`;
     } catch (error) {
-      console.error("Invalid URL:", url);
-      return url; // return as-is if something fails
+        console.error("Invalid URL:", url);
+        return url; // return as-is if something fails
     }
-  }
-  
+}
