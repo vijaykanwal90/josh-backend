f036e53e7e856144aacd778ae9bbc6b824d57d0a
vivekrawat21 <vivekrwt2111@gmail.com>
2025-06-10 16:09:14 +0530

    Add Razorpay integration: implement payment model, create payment routes, and configure Razorpay instance.

---
+++ backend/src/routes/payment.router.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Router } from 'express';
+import { userAuth } from '../middlewares/auth.middleware.js';
+import { createPayment,webHookHandler } from '../controllers/payment.controller.js';
+
+
+const router =  Router();
+
+router.route("/create").post(createPayment);
+router.route("/webhook").post(webHookHandler);
+
+export default router;
